b998cbd4bbdf2dd1eb7507ad5a55de2a0b6df3ef
Sagar63043 <sagarprem6030@gmail.com>
2025-07-19 13:54:02 +0545

    Add all project files

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "formtodb";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("❌ Connection failed: " . $conn->connect_error);
+}
+?>
